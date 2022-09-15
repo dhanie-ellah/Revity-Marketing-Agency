816609e00ea6156614451a4d1caff5fa1a2041b1
dhanie-ellah <101949297+dhanie-ellah@users.noreply.github.com>
2022-09-14 20:26:52 -0700

    process responsive state update

--- script.js
+++ script.js
@@ -13,3 +13,47 @@ closeIcon.addEventListener('click', () => {
      closeIcon.style.display = 'none'
      menuIcon.style.display= 'block'
 })
+
+// const leftBtn = document.querySelector('.fa-angle-left')
+// const rightBtn = document.querySelector('.fa-angle-right')
+// const cards = document.querySelectorAll('.content')
+// let i = 0
+
+// function show (j) {
+//      i += j;
+//      i = Math.min(Math.max(i,0), cards.length--);
+//      cards[i].scrollIntoView({ behavior: 'smooth' });
+// }
+
+// leftBtn.addEventListener('click', () => {
+//      for (let i = 0; i < cards.length; i++) {
+//           const card = array[i];
+//           card--
+//           if (counter < 0) {
+//                counter = cards.length -1
+//           }
+//      }
+// })
+// rightBtn.addEventListener('click', () => {
+//      for (let i = 0; i < cards.length; i++) {
+//           const card = array[i];
+//           card++
+//           if (counter > cards.length) {
+//                counter = 0
+//           }
+//      }
+// })
+
+// rightBtn.addEventListener('click', () => {
+//      counter++
+//      if (counter > cards.length) {
+//           counter = 0
+//      }
+// })
+
+// leftBtn.addEventListener('click', () => {
+//      counter--
+//      if (counter < 0) {
+//           counter = cards.length - 1
+//      }
+// })
