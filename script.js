f7e8494ccdaa9eccb4f76b1d7256c6736c5e5aed
dhanie-ellah <101949297+dhanie-ellah@users.noreply.github.com>
2022-09-28 13:47:50 -0700

    pricing modal added

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ closeIcon.addEventListener('click', () => {
      closeIcon.style.display = 'none'
      menuIcon.style.display = 'block'
 })
+
+const pricing = document.querySelectorAll('.pricing')
+const closePricing = document.querySelectorAll('.close-pricing')
+const modal = document.querySelectorAll('.modal')
+
+pricing.forEach(price => {
+     price.addEventListener('click', () => {
+          modal.forEach(individualModal => {
+               individualModal.style.display = 'flex'
+          })
+     })
+})
+
+closePricing.forEach(close => {
+     close.addEventListener('click', () => {
+          modal.forEach(individualModal => {
+               individualModal.style.display = 'none'
+          })
+     })
+})
